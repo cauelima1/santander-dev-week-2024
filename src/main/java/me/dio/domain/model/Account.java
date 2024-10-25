e87462a44e98446fc801c8cccab62bb0c82977b1
cauelima1 <cauezak@gmail.com>
2024-10-25 12:24:06 -0300

    implementacao da camada de domain

---
+++ src/main/java/me/dio/domain/model/Account.java
@@ -0,0 +1,66 @@
+package me.dio.domain.model;
+
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity(name = "tb_account")
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String number;
+
+    private String agency;
+
+    @Column(nullable = false, precision = 2, scale = 13)
+    private BigDecimal balance;
+
+    @Column(precision = 2, scale = 13)
+    private BigDecimal limite;
+
+    @Column(name = "additional_limit", precision = 2, scale = 13)
+    public BigDecimal getLimite() {
+        return limite;
+    }
+
+    public void setLimite(BigDecimal limite) {
+        this.limite = limite;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public String getAgency() {
+        return agency;
+    }
+
+    public void setAgency(String agency) {
+        this.agency = agency;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
